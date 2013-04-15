e678f5a002045c4029679a0cea9128e3efcb4648
sbocq <sebastien.bocq@gmail.com>
2013-04-15 09:20:06 +0200

    Set micro options to none (they didn't add much)

--- mbench/src/main/scala/mbench/MBench.scala
+++ mbench/src/main/scala/mbench/MBench.scala
@@ -65,16 +65,10 @@ object MBench {
   /**
    * Micro benchmark specific options passed to the cloned JVM.
    *
-   * (default: -Xbatch,-XX::CICompilerCount=1,-Dsun.reflect.inflationThreshold=0)
+   * (default: None)
+   * (ideas: -Xbatch,-XX::CICompilerCount=1,-Dsun.reflect.inflationThreshold=0)
    */
-  val microJvmOptions = Property("micro.jvm.options", properties, Many(
-    // do compilation serially
-    "-Xbatch",
-    // make sure compilation doesn't run in parallel with itself
-    "-XX:CICompilerCount=1",
-    // generate classes or don't, but do it immediately
-    "-Dsun.reflect.inflationThreshold=0"
-  ))
+  val microJvmOptions = Property("micro.jvm.options", properties, Many.empty[String])
 
   /**
    * Print some more information.
